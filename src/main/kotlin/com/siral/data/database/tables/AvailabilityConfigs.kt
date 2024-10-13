2d4a65b7f7d43ab645a0104cb45a70d06803932a
serize02 <ernestoserize@gmail.com>
2024-10-12 23:56:15 -0400

    added-cascade-on-delete-action

--- src/main/kotlin/com/siral/data/database/tables/AvailabilityConfigs.kt
+++ src/main/kotlin/com/siral/data/database/tables/AvailabilityConfigs.kt
@@ -1,10 +1,10 @@
 package com.siral.data.database.tables
 
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
-
 object AvailabilityConfigs: Table("availability_configs") {
-    val dinninghallId = reference("dinninghall_id", Dinninghalls.id)
+    val dinninghallId = long("dinninghall_id").references(Dinninghalls.id, onDelete = ReferenceOption.CASCADE)
     val daysBefore = integer("days_before").default(30)
     override val primaryKey = PrimaryKey(dinninghallId)
 }
--- src/main/kotlin/com/siral/data/database/tables/Reservations.kt
+++ src/main/kotlin/com/siral/data/database/tables/Reservations.kt
@@ -1,13 +1,14 @@
 package com.siral.data.database.tables
 
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object Reservations: Table() {
     val id = long("id").autoIncrement()
-    val studentID = reference("student_id", Students.id)
-    val scheduleItemId = reference("schedule_item_id", Schedule.id)
+    val studentID = long("student_id").references(Students.id, onDelete = ReferenceOption.CASCADE)
+    val scheduleItemId = long("schedule_item_id").references(Schedule.id, onDelete = ReferenceOption.CASCADE)
     val dateOfReservation = datetime("reservation_date").defaultExpression(CurrentDateTime)
     override val primaryKey = PrimaryKey(id)
 }
